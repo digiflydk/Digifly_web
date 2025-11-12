d081f20084900d5e13b0d9677de07e28c45d21b2
Omair Khan <okh2071@gmail.com>
2025-11-12 20:38:20 +0000

    DGF-348 new Error: ## Error Type Build Error
    ## Error Message
    Ecmascrip

--- src/app/dadmin/homepage/actions.ts
+++ src/app/dadmin/homepage/actions.ts
@@ -1,6 +1,5 @@
 
 "use server";
-export const runtime = "nodejs";
 import { saveHomepageAction as saveAction } from "@/lib/server/cms-actions";
 
 export async function saveHomepageAction(payload: unknown) {
