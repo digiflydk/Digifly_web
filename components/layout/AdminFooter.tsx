e7fcf6eb79d9704a79586134a0dff034b53031d9
Omair Khan <okh2071@gmail.com>
2025-11-14 14:14:41 +0000

    DGF-387 where have you implemented the changes

--- components/layout/AdminFooter.tsx
+++ components/layout/AdminFooter.tsx
@@ -2,7 +2,7 @@
 export default function AdminFooter() {
     return (
         <footer className="py-3 px-6 text-center text-xs text-slate-500 border-t bg-white">
-            Version: 1.3.58 • DGF-384
+            Version: 1.3.61 • DGF-387
         </footer>
     )
 }
