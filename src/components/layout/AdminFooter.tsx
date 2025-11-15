42b890e0454cd524ff7714ac89a6f3fbbd601b92
Omair Khan <okh2071@gmail.com>
2025-11-15 12:30:35 +0000

    DGF-415 — Make acceptance project discover a trivial test (sanity check)

--- src/components/layout/AdminFooter.tsx
+++ src/components/layout/AdminFooter.tsx
@@ -2,7 +2,7 @@
 export default function AdminFooter() {
     return (
         <footer className="py-3 px-6 text-center text-xs text-slate-500 border-t bg-white">
-            Version: 1.3.84 • DGF-413
+            Version: 1.3.86 • DGF-415
         </footer>
     )
 }
