427939ba318acfc4e503f8b897b411d596d9bf1d
Omair Khan <okh2071@gmail.com>
2025-11-16 21:16:09 +0000

    ## Error Type Runtime Error
    ## Error Message
    The default export is not

--- src/app/dadmin/developer/audit-logs/_components/AuditLogList.tsx
+++
@@ -1,3 +0,0 @@
-
-// This file has been moved to /dadmin/developer/logs/_components/AuditLogList.tsx
-// This empty file is a placeholder to ensure the old path is deleted.
--- src/app/dadmin/developer/audit-logs/page.tsx
+++
@@ -1,3 +0,0 @@
-
-// This file has been moved to /dadmin/developer/logs/page.tsx
-// This empty file is a placeholder to ensure the old path is deleted.
